07331dcd83ca263c52a820542246bcde6070f172
Dimpho <dimphoset@gmail.com>
2018-07-06 13:30:34 +0200

    Add image posting bot

--- Twitter-bot/config.js
+++ Twitter-bot/config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  consumer_key:         '',
-  consumer_secret:      '',
-  access_token:         '',
-  access_token_secret:  ''/*,
+    consumer_key: 'GcFU0wCQhHevSwGuBOuqkJExI',
+    consumer_secret: 'aBcm7R9JN4dYKp27vclkmv8shjkH1n5FcuKFnzXBR5w0JE4RDA',
+    access_token: '863092381280862209-5HbnzDlEFLEmVmsbvMHITWMAma6XXJh',
+    access_token_secret: '81msWzYkW6oD9p4RQml8dIa8PKjVcM7EKzJ9LPe2OJ7Ik'
+  /*,
   /*timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
   strictSSL:            true,*/
 }
